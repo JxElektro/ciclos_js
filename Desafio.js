bc3cb451b487d50d4f6a7a463dc576a3b1d0dc35
Jhen Nunez <jxelektro@gmail.com>
2022-08-10 10:24:58 -0400

    a

--- Desafio.js
+++ Desafio.js
@@ -180,7 +180,7 @@ function saludo(idioma) {
     }
     return array;
   }
-
+//
   function tieneTresDigitos(numero) {
     //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
     //Escribe tu código aquí
